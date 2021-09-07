11f96a186efb220f1538264b10a533fba1345cc5
Benjamin HERSON-MACAREL <Benjamin.Herson-Macarel-ISC.France@ibm.com>
2021-09-07 22:45:56 +0200

    mopdyfing code for testing purpose

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloaaaad.
+          Edit <code>src/App.js</code> as i am feature.
         </p>
         <a
           className="App-link"
